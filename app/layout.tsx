75064005ea934661c72bebc00f21e42bed614571
Jordi <55429631+jorbush@users.noreply.github.com>
2023-05-28 18:38:33 +0200

    Modal and Button component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
+import ClientOnly from './components/ClientOnly'
+import Modal from './components/modals/Modal'
 
 const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({ subsets: ["latin"] })
@@ -22,7 +24,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar />
+        <ClientOnly>
+          {/*<Modal title="Hello World" actionLabel='Submit' isOpen/>*/}
+          <Navbar />
+        </ClientOnly>
         {children}
       </body>
     </html>
