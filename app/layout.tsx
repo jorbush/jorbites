b4ee425a63ba7b6323b2cadb32fd8bec0b46cf4d
Jordi <55429631+jorbush@users.noreply.github.com>
2023-05-30 09:03:01 +0200

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
 import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
-import Modal from './components/modals/Modal'
+import RegisterModal from './components/modals/RegisterModal'
+import ToasterProvider from './providers/ToasterProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({ subsets: ["latin"] })
@@ -25,7 +26,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          {/*<Modal title="Hello World" actionLabel='Submit' isOpen/>*/}
+          <ToasterProvider/>
+          <RegisterModal />
           <Navbar />
         </ClientOnly>
         {children}
