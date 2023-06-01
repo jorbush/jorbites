27a2476cebea5feedc23ac7a2f9b592f9bc1c6c7
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-01 09:48:39 +0200

    Sign up with GitHub

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -17,6 +17,7 @@ import Modal from "./Modal";
 import Input from "../inputs/Input";
 import Heading from "../Heading";
 import Button from "../Button";
+import { signIn } from "next-auth/react";
 
 const RegisterModal= () => {
   const registerModal = useRegisterModal();
@@ -100,7 +101,7 @@ const RegisterModal= () => {
         outline 
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn('github')}
       />
       <div 
         className="
