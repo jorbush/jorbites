b29c195e9c927204179d35a03f44dbdc2d1d257f
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-01 08:19:58 +0200

    login and getCurrentUser()

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
 import RegisterModal from './components/modals/RegisterModal'
 import ToasterProvider from './providers/ToasterProvider'
+import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
 
 const inter = Inter({ subsets: ['latin'] })
 const font = Nunito({ subsets: ["latin"] })
@@ -17,18 +19,20 @@ export const metadata = {
   },
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider/>
+          <LoginModal/>
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
