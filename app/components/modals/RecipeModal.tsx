6be98b49a77ed3265e2bb1c0c324282730642ad8
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-03 18:44:12 +0200

    CategoryInput and RegisterModal body

--- app/components/modals/RecipeModal.tsx
+++ app/components/modals/RecipeModal.tsx
@@ -3,6 +3,9 @@
 import useRecipeModal from "@/app/hooks/useRecipeModal";
 import Modal from "./Modal"
 import { useMemo, useState } from "react";
+import Heading from "../Heading";
+import { categories } from "../navbar/Categories";
+import CategoryInput from "../inputs/CategoryInput";
 
 enum STEPS {
     CATEGORY = 0,
@@ -39,6 +42,36 @@ const RecipeModal = () => {
         return 'Back'
     }, [step])
 
+    let bodyContent = (
+        <div className="flex flex-col gap-8">
+            <Heading
+                title="Which of these best describes your recipe?"
+                subtitle="Pick a category"
+            />
+            <div 
+                className="
+                grid 
+                grid-cols-1 
+                md:grid-cols-2 
+                gap-3
+                max-h-[50vh]
+                overflow-y-auto
+                "
+            >
+                {categories.map((item) => (
+                    <div key={item.label} className="col-span-1">
+                        <CategoryInput
+                            onClick={() => {}}
+                            selected={false}
+                            label={item.label}
+                            icon={item.icon}
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+
     return (
         <Modal
             isOpen={recipeModal.isOpen}
@@ -46,7 +79,9 @@ const RecipeModal = () => {
             onSubmit={recipeModal.onClose}
             actionLabel={actionLabel}
             secondaryActionLabel={secondaryActionLabel}
+            secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
             title="Post a recipe!"
+            body={bodyContent}
         />
     );
 }
