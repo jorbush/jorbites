551e7736554e7bd13bee50959ec0bfccd8aae6c5
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-03 18:30:46 +0200

    steps and actionlabels

--- app/components/modals/RecipeModal.tsx
+++ app/components/modals/RecipeModal.tsx
@@ -2,15 +2,50 @@
 
 import useRecipeModal from "@/app/hooks/useRecipeModal";
 import Modal from "./Modal"
+import { useMemo, useState } from "react";
+
+enum STEPS {
+    CATEGORY = 0,
+    INGREDIENTS = 1,
+    STEPS = 2,
+    IMAGES = 3,
+    DESCRIPTION = 4
+}
 
 const RecipeModal = () => {
     const recipeModal = useRecipeModal()
+
+    const [step, setStep] = useState(STEPS.CATEGORY)
+
+    const onBack = () => {
+        setStep((value) => value - 1)
+    }
+
+    const onNext = () => {
+        setStep((value) => value + 1)
+    }
+
+    const actionLabel = useMemo(() => {
+        if (step === STEPS.DESCRIPTION) {
+            return 'Create'
+        }
+        return 'Next'
+    }, [step])
+
+    const secondaryActionLabel = useMemo(() => {
+        if (step === STEPS.DESCRIPTION) {
+            return undefined
+        }
+        return 'Back'
+    }, [step])
+
     return (
         <Modal
             isOpen={recipeModal.isOpen}
             onClose={recipeModal.onClose}
             onSubmit={recipeModal.onClose}
-            actionLabel="Submit"
+            actionLabel={actionLabel}
+            secondaryActionLabel={secondaryActionLabel}
             title="Post a recipe!"
         />
     );
