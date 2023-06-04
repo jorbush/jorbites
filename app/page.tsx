27dd761d71658b72b01be4da2f136eec2674d1a0
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-04 19:39:53 +0200

    getListings()

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,50 @@
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
 
-export default function Home() {
+import getListings from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
+
+
+const Home = async () => {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
       <Container>
-        <div className='
-          pt-24
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        '> 
-          <div>My future listings</div>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              key={listing.id}
+              
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   )
 }
+
+export default Home;
