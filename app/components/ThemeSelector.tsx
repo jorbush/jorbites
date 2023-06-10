836ed9d8e83bd80d16c42b2db39513d5d5b78daa
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-10 12:41:01 +0200

    Toggle dark theme

--- app/components/ThemeSelector.tsx
+++ app/components/ThemeSelector.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import useTheme from '../hooks/useTheme';
+import { HiMoon, HiSun } from 'react-icons/hi';
 
 const ThemeSelector: React.FC = () => {
   const [theme, setTheme] = useState<string>('light');
@@ -24,9 +24,41 @@ const ThemeSelector: React.FC = () => {
   }, [theme]);
 
   return (
-    <button onClick={toggleTheme}>
-      Cambiar a {theme === 'light' ? 'dark' : 'light'}
-    </button>
+    <div className="flex items-center">
+      <div className="flex-1">
+        <p className="text-left">{theme === 'dark' ? 'Enable light theme' : 'Enable dark theme'}</p>
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={toggleTheme}
+          className="relative inline-flex flex-shrink-0 h-6 w-11 rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-0 border border-gray-50"
+        >
+          <span className="sr-only">Toggle</span>
+          <span
+            className={`${
+              theme === 'dark' ? 'translate-x-5' : 'translate-x-0'
+            } relative inline-block h-5 w-5 mt-[1.2px] ml-[1.2px] rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200`}
+          >
+            <span
+              className={`${
+                theme === 'dark' ? 'opacity-0 ease-out duration-100' : 'opacity-100 ease-in duration-200'
+              } absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+              aria-hidden="true"
+            >
+              <HiSun className="h-3 w-3 text-gray-400" />
+            </span>
+            <span
+              className={`${
+                theme === 'dark' ? 'opacity-100 ease-in duration-200' : 'opacity-0 ease-out duration-100'
+              } absolute inset-0 h-full w-full flex items-center justify-center transition-opacity`}
+              aria-hidden="true"
+            >
+              <HiMoon className="h-3 w-3 text-gray-400" />
+            </span>
+          </span>
+        </button>
+      </div>
+    </div>
   );
 };
 
