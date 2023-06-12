a30bed8331ae9bb878255fd1285f472432fb79cb
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-12 12:40:35 +0200

    CommentBox

--- app/components/comments/Comments.tsx
+++ app/components/comments/Comments.tsx
@@ -1,12 +1,36 @@
 "use client";
 
-import CommentInput from "./CommentInput";
+import { SafeUser } from "@/app/types";
+import CommentBox from "./CommentBox";
 
+interface CommentsProps {
+  currentUser?: SafeUser | null;
+}
 
-const Comments = () => {
+const Comments: React.FC<CommentsProps> = ({ currentUser }) => {
     return (
-        <div>
-            <CommentInput/>
+        <div className="
+            flex
+            flex-col
+            pr-2
+            pl-2
+        ">
+            <hr/>
+            <div 
+                className="
+                text-xl 
+                font-semibold 
+                flex 
+                flex-row 
+                items-center
+                gap-2
+                mb-4
+                mt-8
+                "
+            >
+                Comments
+            </div>
+            <CommentBox userImage={currentUser?.image}/>
             {/* TODO: LIST COMMENTS */}
         </div>
     );
