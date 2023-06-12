1f2f1d9175d9134b5b68207a54bdda0981b30348
Jordi <55429631+jorbush@users.noreply.github.com>
2023-06-12 18:47:41 +0200

    Comment layout updated

--- app/components/comments/Comment.tsx
+++ app/components/comments/Comment.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import React from 'react';
+import { format } from 'date-fns';
 import Avatar from '../Avatar';
 
 interface CommentProps {
@@ -8,17 +11,22 @@ interface CommentProps {
 }
 
 const Comment: React.FC<CommentProps> = ({ userImage, comment, createdAt }) => {
+  const formattedDate = format(new Date(createdAt), 'dd/MM/yyyy HH:mm');
+
   return (
-    <div className="flex items-start mb-4 ml-4 mr-4">
+    <div className="flex items-start m-4">
       <div className="flex-shrink-0 mt-2">
         <Avatar src={userImage} />
       </div>
-      <div className="ml-4">
+      <div className="ml-4 flex-grow">
         <p className="text-gray-800">{comment}</p>
-        <p className="text-gray-500 text-sm">{createdAt}</p>
+        <div className="flex flex-col text-gray-400 text-sm">
+          {formattedDate}
+        </div>
       </div>
     </div>
   );
 };
 
 export default Comment;
+
