2594b2a7e0af334ab011e54a0de0dbb7cc01c661
Jordi <55429631+jorbush@users.noreply.github.com>
2024-07-10 09:48:18 +0200

    Distinguish between mobile and pc when getting the recipes

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-export const dynamic = 'force-dynamic'
-
 import Container from "@/app/components/Container";
 import ListingCard from "@/app/components/listings/ListingCard";
 import EmptyState from "@/app/components/EmptyState";
 import Pagination from "@/app/components/Pagination";
-
+import { isMobile as detectMobile } from "./utils/deviceDetector";
 import getListings, { IListingsParams } from "@/app/actions/getListings";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import ClientOnly from "./components/ClientOnly";
+import { headers } from 'next/headers';
 
 interface HomeProps {
   searchParams: IListingsParams
 };
 
 const Home = async ({ searchParams }: HomeProps) => {
-  const listingsData = await getListings(searchParams);
+  const headersList = headers();
+  const userAgent = headersList.get('user-agent') || '';
+  const isMobile = detectMobile(userAgent);
+  const limit = isMobile ? 6 : 10;
+  const listingsData = await getListings({ ...searchParams, limit });
   const currentUser = await getCurrentUser();
 
   if (listingsData.listings.length === 0) {
