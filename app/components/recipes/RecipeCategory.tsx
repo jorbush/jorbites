a1d7d32bac7a08a9f9da6d052363242e7b161d41
Jordi <55429631+jorbush@users.noreply.github.com>
2024-07-25 08:37:57 +0200

    RecipeCategory

--- app/components/recipes/RecipeCategory.tsx
+++ app/components/recipes/RecipeCategory.tsx
@@ -16,11 +16,18 @@ const RecipeCategoryView: React.FC<CategoryViewProps> = ({
 }) => {
     const { t } = useTranslation();
     return (
-        <div className="flex flex-col gap-6">
-            <div className="flex flex-row items-center gap-4">
+        <div
+            className="flex flex-col gap-6"
+            data-testid="recipe-category-view"
+        >
+            <div
+                className="flex flex-row items-center gap-4"
+                data-testid="recipe-category-inner"
+            >
                 <Icon
                     size={40}
                     className="text-neutral-600"
+                    data-testid={`${Icon.name.toLowerCase()}`}
                 />
                 <div className="flex flex-col">
                     <div className="text-lg font-semibold">
