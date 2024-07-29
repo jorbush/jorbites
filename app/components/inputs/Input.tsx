5a7081f6a8ac52e99116d0a6ba95fd58e50b8a00
Jordi <55429631+jorbush@users.noreply.github.com>
2024-07-29 09:24:30 +0200

    test LoginModal

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -45,6 +45,7 @@ const Input: React.FC<InputProps> = ({
                 className={`peer w-full rounded-md border-2 bg-white p-4 pt-6 font-light outline-none transition disabled:cursor-not-allowed disabled:opacity-70 dark:text-dark ${formatPrice ? 'pl-9' : 'pl-4'} ${errors[id] ? 'border-rose-500' : 'border-neutral-300'} ${errors[id] ? 'focus:border-rose-500' : 'focus:border-black'} `}
             />
             <label
+                htmlFor={id}
                 className={`text-md absolute top-5 z-10 origin-[0] -translate-y-3 transform duration-150 ${formatPrice ? 'left-9' : 'left-4'} peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:-translate-y-4 peer-focus:scale-75 ${errors[id] ? 'text-rose-500' : 'text-zinc-400'} `}
             >
                 {label}
