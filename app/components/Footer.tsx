0539e2a7077037d8a8e42fb4f2471489373b6832
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-13 09:20:54 +0200

    add policies links to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,17 +1,24 @@
 'use client';
 
 import { useTranslation } from 'react-i18next';
-import useTheme from '../hooks/useTheme';
+import useTheme from '@/app/hooks/useTheme';
+import Link from 'next/link';
 
 const Footer = () => {
     const { t } = useTranslation();
-
     useTheme();
-
     return (
         <div className="flex w-full flex-col items-center justify-center p-4 text-neutral-200 dark:text-gray-600">
             <div>{`${t('version')} 0.5`}</div>
             <div>{`${t('contact')}: jbonetv5@gmail.com`}</div>
+            <div className="flex flex-row items-center space-y-0 space-x-4">
+                <Link href="/policies/privacy" className="hover:underline">
+                    {t('privacy_policy')}
+                </Link>
+                <Link href="/policies/cookies" className="hover:underline">
+                    {t('cookies_policy')}
+                </Link>
+            </div>
         </div>
     );
 };
