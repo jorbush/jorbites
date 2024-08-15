da84e15f48dce85c5209c801b47887d2c00ac4c3
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-15 22:39:13 +0200

    add more config

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,10 +2,19 @@ module.exports = {
     ci: {
         collect: {
             url: ['http://localhost:3000/'],
-            startServerCommand: 'npm run dev',
+            startServerCommand: 'npm run start',
+            startServerReadyPattern: "ready on",
+            numberOfRuns: 1,
+            settings: {
+                preset: "desktop"
+            }
+        },
+        assert: {
+            preset: "lighthouse:recommended"
         },
         upload: {
             target: 'temporary-public-storage',
         },
+        server: {}
     },
 };
