414a3a82f0f3640ccd07905e520e9064a8ee8b18
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-15 22:45:49 +0200

    use perf

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,7 +4,10 @@ module.exports = {
             url: ['http://localhost:3000/'],
             startServerCommand: 'npm run start',
             startServerReadyPattern: "ready on",
-            numberOfRuns: 1
+            numberOfRuns: 1,
+            settings: {
+                preset: "perf"
+            }
         },
         assert: {
             preset: "lighthouse:recommended"
