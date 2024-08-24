e72ec88e8fd9761cabc87e4b59feab0781bccc16
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-24 13:58:52 +0200

    without toast

--- __tests__/e2e/user.cy.ts
+++ __tests__/e2e/user.cy.ts
@@ -12,7 +12,6 @@ describe('User', () => {
             Cypress.env('userTestPassword')
         );
         cy.get('[data-cy="modal-action-button"]').click();
-        cy.get('[class^="go"]', { timeout: 30000 }).should('be.visible');
         // Logout
         cy.get('[data-cy="user-menu"]').click();
         cy.get('[data-cy="user-menu-logout"]').click();
