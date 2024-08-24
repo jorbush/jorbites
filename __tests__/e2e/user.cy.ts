ed136d6379d2a4931cfb3ac0fafdee1cd38e4254
Jordi <55429631+jorbush@users.noreply.github.com>
2024-08-24 15:24:28 +0200

    try

--- __tests__/e2e/user.cy.ts
+++ __tests__/e2e/user.cy.ts
@@ -5,19 +5,23 @@ describe('User', () => {
         cy.get('.text-lg').eq(0).should('have.text', 'a');
 
         // Login
-        cy.get('[data-cy="user-menu"]').click();
-        cy.get('[data-cy="user-menu-login"]').should('be.visible').click();
+        cy.get('[data-cy="user-menu"]', { timeout: 10000 }).should('be.visible').click();
+        cy.get('[data-cy="user-menu-login"]', { timeout: 10000 }).should('be.visible').click();
         cy.get('[data-cy="login-email"]').type(Cypress.env('userTestEmail'));
-        cy.get('[data-cy="login-password"]').type(
-            Cypress.env('userTestPassword')
-        );
+        cy.get('[data-cy="login-password"]').type(Cypress.env('userTestPassword'));
         cy.get('[data-cy="modal-action-button"]').click();
-        cy.get('body').should('not.contain', 'Invalid credentials');
-        cy.get('[data-cy="login-modal"]').should('not.exist');
+
+        // Wait for login process to complete
+        cy.wait(5000);
+
         // Logout
-        cy.get('[data-cy="user-menu"]').click();
-        cy.get('[data-cy="user-menu-logout"]').click();
-        cy.get('[data-cy="user-menu"]').click();
+        cy.get('[data-cy="user-menu"]', { timeout: 10000 }).should('be.visible').click({ force: true });
+        cy.get('[data-cy="user-menu-logout"]', { timeout: 10000 }).should('be.visible').click({ force: true });
+
+        // Wait for logout process to complete
+        cy.wait(20000);
+
+        cy.get('[data-cy="user-menu"]', { timeout: 10000 }).should('be.visible').click({ force: true });
         cy.get('[data-cy="user-menu-logout"]').should('not.exist');
         cy.get('[data-cy="user-menu-login"]').should('be.visible');
     });
