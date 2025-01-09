692f9d1e5fe6d9e184e3d2f5cd15ac122aab8f11
Jordi <55429631+jorbush@users.noreply.github.com>
2025-01-09 18:25:49 +0100

    remove import

--- __tests__/unit_test/components/recipes/RecipeInfo.test.tsx
+++ __tests__/unit_test/components/recipes/RecipeInfo.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
-import { describe, it, expect, vi, afterEach, beforeEach } from 'vitest';
+import { describe, it, expect, vi, afterEach } from 'vitest';
 import RecipeInfo from '@/app/components/recipes/RecipeInfo';
 import { SafeUser } from '@/app/types';
 import * as nextNavigation from 'next/navigation';
