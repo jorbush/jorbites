f45c1c82bf7810e3a7ca2bb4bbb698b4d7391a53
Jordi <55429631+jorbush@users.noreply.github.com>
2025-02-17 21:20:50 +0100

    isNew prop

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,20 +1,25 @@
 'use client';
+import { MdFiberNew } from "react-icons/md";
 
 interface MenuItemProps {
     onClick: () => void;
     label: string;
     props?: string;
+    isNew?: boolean;
     dataCy?: string;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ onClick, label, props, dataCy }) => {
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label, props, isNew, dataCy }) => {
     return (
         <div
             onClick={onClick}
             className={`px-4 py-3 font-semibold transition hover:bg-neutral-100 hover:text-black ${props}`}
             data-cy={dataCy}
         >
-            {label}
+            <div className="flex items-center justify-between">
+                {label}
+                {isNew && <MdFiberNew className="text-green-450 " size={25} />}
+            </div>
         </div>
     );
 };
