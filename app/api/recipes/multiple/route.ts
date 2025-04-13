388e389bd0d3083a4511a29b8bc380ce8d28d553
Jordi <55429631+jorbush@users.noreply.github.com>
2025-04-13 19:27:20 +0200

    fix unintended check in multiple endpoints

--- app/api/recipes/multiple/route.ts
+++ app/api/recipes/multiple/route.ts
@@ -1,14 +1,7 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/app/libs/prismadb';
-import getCurrentUser from '@/app/actions/getCurrentUser';
 
 export async function GET(request: Request) {
-    const currentUser = await getCurrentUser();
-
-    if (!currentUser) {
-        return NextResponse.error();
-    }
-
     try {
         const url = new URL(request.url);
         const idsParam = url.searchParams.get('ids');
