f7b84778f4bc16ba576998fc3252a1ee40a35b07
Jordi <55429631+jorbush@users.noreply.github.com>
2025-04-14 08:11:48 +0200

    improve dark theme IOS PWA

--- app/hooks/useTheme.ts
+++ app/hooks/useTheme.ts
@@ -8,8 +8,47 @@ const useTheme = () => {
                 'dark',
                 cachedTheme === 'dark'
             );
+
+            updateStatusBarStyle(cachedTheme === 'dark');
         }
+
+        document.addEventListener('themeChanged', (e) => {
+            const isDark = (e as CustomEvent).detail.isDark;
+            updateStatusBarStyle(isDark);
+        });
+
+        return () => {
+            document.removeEventListener('themeChanged', () => {});
+        };
     }, []);
+
+    const updateStatusBarStyle = (isDark: boolean) => {
+        let statusBarMeta = document.querySelector(
+            'meta[name="apple-mobile-web-app-status-bar-style"]'
+        );
+
+        if (!statusBarMeta) {
+            statusBarMeta = document.createElement('meta');
+            statusBarMeta.setAttribute(
+                'name',
+                'apple-mobile-web-app-status-bar-style'
+            );
+            document.head.appendChild(statusBarMeta);
+        }
+        statusBarMeta.setAttribute(
+            'content',
+            isDark ? 'black-translucent' : 'default'
+        );
+        let themeColorMeta = document.querySelector('meta[name="theme-color"]');
+
+        if (!themeColorMeta) {
+            themeColorMeta = document.createElement('meta');
+            themeColorMeta.setAttribute('name', 'theme-color');
+            document.head.appendChild(themeColorMeta);
+        }
+
+        themeColorMeta.setAttribute('content', isDark ? '#0F0F0F' : '#ffffff');
+    };
 };
 
 export default useTheme;
