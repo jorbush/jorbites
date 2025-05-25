ff15212eb0e755492f1e3b9755b5c9d1676aba35
Jordi <55429631+jorbush@users.noreply.github.com>
2025-05-25 13:13:27 +0200

    add missing translation favorites

--- app/favorites/FavoritesClient.tsx
+++ app/favorites/FavoritesClient.tsx
@@ -1,8 +1,10 @@
+'use client';
+
 import { SafeRecipe, SafeUser } from '@/app/types';
-import Heading from '@/app/components/navigation/Heading';
 import Container from '@/app/components/utils/Container';
 import RecipeCard from '@/app/components/recipes/RecipeCard';
 import { FcLike } from 'react-icons/fc';
+import { useTranslation } from 'react-i18next';
 
 interface FavoritesClientProps {
     recipes: SafeRecipe[];
@@ -13,12 +15,14 @@ const FavoritesClient: React.FC<FavoritesClientProps> = ({
     recipes,
     currentUser,
 }) => {
+    const { t } = useTranslation();
+
     return (
         <Container>
             <div className="mb-10 text-center">
                 <h1 className="mb-3 flex items-center justify-center text-3xl font-bold sm:text-4xl dark:text-neutral-100">
                     <FcLike className="mr-2 text-3xl sm:text-4xl" />
-                    Favorites
+                    {t('favorites')}
                 </h1>
             </div>
             <div className="mt-10 grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
