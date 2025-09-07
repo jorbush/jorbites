83fca242b41c3f16b31ebd1d6cb87850a8a091c8
Jordi <55429631+jorbush@users.noreply.github.com>
2025-09-07 13:10:41 +0200

    fix hr recipe info cat and method

--- app/components/recipes/RecipeCategoryAndMethod.tsx
+++ app/components/recipes/RecipeCategoryAndMethod.tsx
@@ -25,7 +25,7 @@ const RecipeCategoryAndMethod: React.FC<RecipeInfoProps> = ({
 }) => {
     return (
         <>
-            <hr />
+            {category || (method && <hr />)}
             <div
                 data-testid="recipe-category-and-method"
                 className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2 dark:text-neutral-100"
