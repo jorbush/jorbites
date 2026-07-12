577331a10e12822a6b08ee79468243e3e1722cbb
Jordi <55429631+jorbush@users.noreply.github.com>
2026-07-12 13:03:33 +0200

    optimize axiom logs

--- app/lib/axiom/client.ts
+++ app/lib/axiom/client.ts
@@ -1,13 +1,29 @@
 'use client';
 
 import { Logger, ProxyTransport } from '@axiomhq/logging';
-import { createUseLogger, createWebVitalsComponent } from '@axiomhq/react';
+import { createUseLogger } from '@axiomhq/react';
+
+const sanitizeFieldsFormatter = (logEvent: any) => {
+    if (logEvent.fields) {
+        const sanitizedFields = { ...logEvent.fields };
+        for (const [key, value] of Object.entries(sanitizedFields)) {
+            if (value && typeof value === 'object') {
+                sanitizedFields[key] = JSON.stringify(value);
+            }
+        }
+        return {
+            ...logEvent,
+            fields: sanitizedFields,
+        };
+    }
+    return logEvent;
+};
 
 export const logger = new Logger({
     transports: [new ProxyTransport({ url: '/api/axiom', autoFlush: true })],
+    formatters: [sanitizeFieldsFormatter],
 });
 
 const useLogger = createUseLogger(logger);
-const WebVitals = createWebVitalsComponent(logger);
 
-export { useLogger, WebVitals };
+export { useLogger };
--- app/lib/axiom/server.ts
+++ app/lib/axiom/server.ts
@@ -2,6 +2,22 @@ import axiomClient from '@/app/lib/axiom/axiom';
 import { Logger, AxiomJSTransport } from '@axiomhq/logging';
 import { nextJsFormatters } from '@axiomhq/nextjs';
 
+const sanitizeFieldsFormatter = (logEvent: any) => {
+    if (logEvent.fields) {
+        const sanitizedFields = { ...logEvent.fields };
+        for (const [key, value] of Object.entries(sanitizedFields)) {
+            if (value && typeof value === 'object') {
+                sanitizedFields[key] = JSON.stringify(value);
+            }
+        }
+        return {
+            ...logEvent,
+            fields: sanitizedFields,
+        };
+    }
+    return logEvent;
+};
+
 export const logger = new Logger({
     transports: [
         new AxiomJSTransport({
@@ -9,5 +25,5 @@ export const logger = new Logger({
             dataset: process.env.AXIOM_DATASET!,
         }),
     ],
-    formatters: nextJsFormatters,
+    formatters: [...nextJsFormatters, sanitizeFieldsFormatter],
 });
